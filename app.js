354b9c6983453ebf25ec9f8873e6a439d4d8df1a
Aki Guanzon <83754370+akiguanzon@users.noreply.github.com>
2022-10-12 00:21:25 +0800

    0.1.3

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ function rightScroll() {
     right.scrollBy(500, 0);
 }
 
+function leftScroll2() {
+    const left = document.querySelector("#scrolling-wrapper-latest");
+    left.scrollBy(-500, 0);
+}
+function rightScroll2() {
+    const right = document.querySelector("#scrolling-wrapper-latest");
+    right.scrollBy(500, 0);
+}
+
 
 $(function () {
     var scrollLeftPrev = 0;
@@ -33,3 +42,33 @@ $(function () {
         scrollLeftPrev = newScrollLeft;
     });
 });
+
+$(function () {
+    var scrollLeftPrev = 0;
+    $('#scrolling-wrapper-latest').scroll(function () {
+        // console.log($('#scrollquestion').width());
+        // console.log($('#scrollquestion').scrollLeft());
+        var $elem = $('#scrolling-wrapper-latest');
+        var newScrollLeft = $elem.scrollLeft(),
+            width = $elem.outerWidth(),
+            scrollWidth = $elem.get(0).scrollWidth;
+        if (scrollWidth - newScrollLeft == width) {
+            document.querySelector('#latest-right').classList.add('button-gone');
+        }
+        else {
+            document.querySelector('#latest-right').classList.remove('button-gone');
+        }
+        if (newScrollLeft === 0) {
+            document.querySelector('#latest-left').classList.add('button-gone');
+        }
+        else {
+            document.querySelector('#latest-left').classList.remove('button-gone');
+        }
+
+        scrollLeftPrev = newScrollLeft;
+    });
+});
+
+ScrollReveal().reveal('.featured-post', { delay: 100 })
+ScrollReveal().reveal('.scroll-case', { delay: 100 })
+ScrollReveal().reveal('.box-divider', { delay: 100 })
